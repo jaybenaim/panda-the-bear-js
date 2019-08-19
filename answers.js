21061b034495b96cf2edd3375442611ae7fede70
jaybenaim <benaimjacob@gmail.com>
2019-08-19 14:03:05 -0400

    added jquery to head

---
+++ answers.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(e) {
+    let head = document.querySelector('head')
+    head.innerHTML("<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>")
+    $('h1.highlight').html("<div>New TEXT </div>")
+    
+}); 
