cdfe0d3663b0fbdd657975d4e87c40e17c20ea7d
jaybenaim <benaimjacob@gmail.com>
2019-08-19 14:47:24 -0400

    changed employment heading

--- answers.js
+++ answers.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(e) {
-    let head = document.querySelector('head')
-    head.innerHTML("<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>")
-    $('h1.highlight').html("<div>New TEXT </div>")
+    // Add jQuery 
+    $('head').append('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>') 
+    // test 
+    $('h1.highlight').html("<div>New TEXT </div>") 
+    // Question 1 
     $('.profile-image').attr('src', "https://www.denverzoo.org/wp-content/uploads/2018/09/Grizzly-Bear_Thumb.jpg") 
-    $('.portfolio-image img').attr('src' , "https://66.media.tumblr.com/2f14f2062c1a9c5cc834aa88140d944c/tumblr_mw8evbd6t71r2yc90o1_400.png")
+    // Question 2 
+    $('.portfolio-image img').first().attr('src' , "https://i.pinimg.com/originals/61/e5/f2/61e5f286f62029ee0b42483031cd0e64.png")
+    // Question 3 
+    $('.bio-info-value').first().text('JayJay Benaim')
+    // or $('.bio-info-name').text("Jay Benaim")
+    // Question 4
+    $('.info-inner-container h3').text('You got changed!')
     
+
 }); 
